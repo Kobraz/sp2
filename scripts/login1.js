63c2b389347fa82bc9c40a828274f1254998da80
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-17 20:06:02 +0100

    added scripts

---
+++ scripts/login1.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const email = e.currentTarget[0].value;
+  const password = e.currentTarget[1].value;
+
+  fetch(
+    `https://api.noroff.dev/api/v1/auction/auth/login?email=${email}&password=${password}`,
+    {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("Failed to fetchdata from API");
+      }
+    })
+    .then((data) => {
+      if (data.exists) {
+        console.log("Data exists:", data);
+      } else {
+        console.log("Data does not exist");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      console.error("Error fetching data from API: ", err);
+    });
+});
---
+++ scripts/register.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = e.currentTarget[0].value;
+  const email = e.currentTarget[1].value;
+  const password = e.currentTarget[2].value;
+  // debugger;
+  const payload = {
+    name,
+    email,
+    password,
+  };
+
+  // console.log( typeof payload)
+  // const prePayload = new FormData(form);
+  // const payload = new URLSearchParams(prePayload);
+
+  // console.log(payload);
+
+  fetch("https://api.noroff.dev/api/v1/auction/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
