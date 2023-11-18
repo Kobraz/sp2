c4af7cf5cf093e6d11ec5ef9a250199f47f7cc97
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-18 12:35:34 +0100

    updated register.js

--- scripts/register.js
+++ scripts/register.js
@@ -19,13 +19,13 @@ form.addEventListener("submit", function (e) {
   // console.log(payload);
 
   fetch("https://api.noroff.dev/api/v1/auction/auth/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-});
+ method: "POST",
+ headers: {
+    "Content-Type": "application/json",
+    "Content-Length": JSON.stringify(payload).length,
+ },
+ body: JSON.stringify(payload),
+})
+ .then((res) => res.json())
+ .then((data) => console.log(data))
+ .catch((err) => console.log(err));
