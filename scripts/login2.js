e564ce808c1dc2eed3e1a1c51548569e85bddb3d
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-19 13:57:50 +0100

    updated the register and login files

---
+++ scripts/login2.js
@@ -0,0 +1,30 @@
+fetch("https://api.noroff.dev/api/v1/auction/auth/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    email: email,
+    password: password,
+  }),
+})
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Failed to fetch data from API");
+    }
+  })
+  .then((data) => {
+    if (data.exists) {
+      console.log("Data exists:", data);
+      // You may want to set the Authorization header here,
+      // depending on your server-side implementation
+    } else {
+      console.log("Data does not exist");
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+    console.error("Error fetching data from API: ", err);
+  });
