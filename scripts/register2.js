3f11e362d1917cf30d6152b9f4d0a5a4bcff10d6
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-24 10:10:39 +0100

    updated register2.js

--- scripts/register2.js
+++ scripts/register2.js
@@ -14,7 +14,7 @@ document.getElementById("form").addEventListener("submit", function (e) {
   };
 
   fetch("https://api.noroff.dev/api/v1/auction/auth/register", {
-    method: "get",
+    method: "post",
     headers: {
       "Content-Type": "application/json",
     },
