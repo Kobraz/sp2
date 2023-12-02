985ec7bd5b67a911ffb8fc98636fb696b83b0f6d
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-02 15:55:19 +0100

    added dashboard.js

---
+++ scripts/dashboard.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  let userName = localStorage.getItem("name");
+  let avatar = localStorage.getItem("avatar");
+  let email = localStorage.getItem("email");
+  let credit = localStorage.getItem("credit");
+
+  if (userName) {
+    document.getElementById("userName").innerHTML = userName;
+  }
+
+  if (avatar) {
+    document.getElementById("avatar").src = avatar;
+  }
+
+  if (email) {
+    document.getElementById("email").innerHTML = email;
+  }
+
+  if (credit) {
+    document.getElementById("credit").innerHTML = credit;
+  }
+};
