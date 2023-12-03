25447a7c0b1c7b29ea2de60250e035e40eac23e9
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-03 17:34:06 +0100

    updated hambuger.js

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addeventlistener("click", () => {
-  hamburger.classlist.toggle("active");
-  navMenu.classlist.toggle("active");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
