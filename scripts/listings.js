431f24e6785fd8aeb3fc5592df8b1ed43e653ab7
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-04 00:06:28 +0100

    added listings.js and updated dashboard.html

---
+++ scripts/listings.js
@@ -0,0 +1,18 @@
+fetch("https://api.noroff.dev/api/v1/auction/listings")
+  .then((data) => {
+    return data.json();
+  })
+  .then((objectData) => {
+    console.log(objectData[0].title);
+    let tableData = "";
+    objectData.map((values) => {
+      tableData += ` <tr>
+        <td><a href="details.html?id=${values.id}">${values.title}</a></td>
+        <td>${values.description}</td>
+        <td><img src="${values.media}"></td>
+        </tr>`;
+    });
+    document.getElementById("tableBody").innerHTML = tableData;
+
+    //const apiArray = [${values.id},${values.title}, ${values.description}, ${values.price}, ${values.category}, ${values.rating}, ${values.image}]
+  });
