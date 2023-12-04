bef2e48e151d86a6d1901c18b74bc7c31bef1c6e
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-04 12:53:52 +0100

    upd dashboard.html,and listings.js created copy

--- scripts/listings.js
+++ scripts/listings.js
@@ -1,45 +1,21 @@
-// Function to generate table row HTML code
-function generateTableRow(values) {
-  let mediaHTML = "";
-  if (Array.isArray(values.media)) {
-    values.media.forEach((mediaUrl) => {
-      mediaHTML += `<img src="${mediaUrl}" alt="image">`;
+fetch("https://api.noroff.dev/api/v1/auction/listings")
+  .then((response) => response.json())
+  .then((data) => {
+    const cardTemplate = document.querySelector(".card");
+    const tableBody = document.querySelector("#tableBody");
+
+    data.forEach((item) => {
+      const card = cardTemplate.cloneNode(true);
+      card.querySelector("#cardHeader").textContent = item.title;
+      card.querySelector("#cardDescription").textContent = item.description;
+      card.querySelector("#cardMedia").textContent = item.media;
+      card.querySelector("#cardTags").textContent = item.tags;
+      card.querySelector("#cardCreated").textContent = item.created;
+      card.querySelector("#cardUpdated").textContent = item.updated;
+
+      tableBody.appendChild(card);
     });
-  } else {
-    mediaHTML = `<img src="${values.media}" alt="image">`;
-  }
-
-  return ` <tr>
-     <td><a href="details.html?id=${values.id}">${values.title}</a></td>
-     <td>${values.description}</td>
-     <td>${mediaHTML}</td>
-     <td>${values.tags}</td>
-     <td>${values.created}</td>
-     <td>${values.updated}</td>
-     </tr>`;
-}
-
-// Function to populate table
-function populateTable(objectData) {
-  let tableData = "";
-  objectData.forEach((values) => {
-    tableData += generateTableRow(values);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
   });
-  document.getElementById("tableBody").innerHTML = tableData;
-}
-
-// Function to fetch data from API and populate table
-function fetchAndPopulateTable() {
-  fetch("https://api.noroff.dev/api/v1/auction/listings")
-    .then((data) => {
-      return data.json();
-    })
-    .then((objectData) => {
-      populateTable(objectData);
-    });
-}
-
-// Run fetchAndPopulateTable function when the DOM has been loaded
-document.addEventListener("DOMContentLoaded", function () {
-  fetchAndPopulateTable();
-});
