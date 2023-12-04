95f3bd146e92d8e83e2becff8df0ad94af1a67d5
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-04 12:33:09 +0100

    upd listings.js

--- scripts/listings.js
+++ scripts/listings.js
@@ -1,21 +1,22 @@
-fetch("https://api.noroff.dev/api/v1/auction/listings")
-  .then((data) => {
-    return data.json();
-  })
-  .then((objectData) => {
-    let tableData = "";
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://api.noroff.dev/api/v1/auction/listings")
+    .then((data) => {
+      return data.json();
+    })
+    .then((objectData) => {
+      let tableData = "";
 
-    objectData.forEach((values) => {
-      let mediaHTML = "";
-      if (Array.isArray(values.media)) {
-        values.media.forEach((mediaUrl) => {
-          mediaHTML += `<img src="${mediaUrl}" alt="image">`;
-        });
-      } else {
-        mediaHTML = `<img src="${values.media}" alt="image">`;
-      }
+      objectData.forEach((values) => {
+        let mediaHTML = "";
+        if (Array.isArray(values.media)) {
+          values.media.forEach((mediaUrl) => {
+            mediaHTML += `<img src="${mediaUrl}" alt="image">`;
+          });
+        } else {
+          mediaHTML = `<img src="${values.media}" alt="image">`;
+        }
 
-      tableData += ` <tr>
+        tableData += ` <tr>
         <td><a href="details.html?id=${values.id}">${values.title}</a></td>
         <td>${values.description}</td>
         <td>${mediaHTML}</td>
@@ -23,23 +24,31 @@ fetch("https://api.noroff.dev/api/v1/auction/listings")
         <td>${values.created}</td>
         <td>${values.updated}</td>
         </tr>`;
-    });
-    document.getElementById("tableBody").innerHTML = tableData;
+      });
+      document.getElementById("tableBody").innerHTML = tableData;
 
-    cardHeader.innerHTML += `${values.title}`;
+      document.getElementById("cardHeader").innerHTML = values.title;
+      document.getElementById("cardDescription").innerHTML = values.description;
+      document.getElementById("cardMedia").innerHTML = mediaHTML;
+      document.getElementById("cardTags").innerHTML = values.tags;
+      document.getElementById("cardCreated").innerHTML = values.created;
+      document.getElementById("cardUpdated").innerHTML = values.updated;
+
+      /* cardHeader.innerHTML += `${values.title}`;
     cardDescription.innerHTML += `${values.description}`;
     cardMedia.innerHTML += `${mediaHTML}`;
     cardTags.innerHTML += `${values.tags}`;
     cardCreated.innerHTML += `${values.created}`;
-    cardUpdated.innerHTML += `${values.updated}`;
+    cardUpdated.innerHTML += `${values.updated}`; */
 
-    console.log("Id: " + objectData[0].id);
-    console.log("Title: " + objectData[0].title);
-    console.log("Description: " + objectData[0].description);
-    console.log("Media: " + objectData[0].media);
-    console.log("Tags: " + objectData[0].tags);
-    console.log("Created: " + objectData[0].created);
-    console.log("Updated: " + objectData[0].updated);
+      console.log("Id: " + objectData[0].id);
+      console.log("Title: " + objectData[0].title);
+      console.log("Description: " + objectData[0].description);
+      console.log("Media: " + objectData[0].media);
+      console.log("Tags: " + objectData[0].tags);
+      console.log("Created: " + objectData[0].created);
+      console.log("Updated: " + objectData[0].updated);
 
-    console.log("tableData: " + tableData);
-  });
+      console.log("tableData: " + tableData);
+    });
+});
