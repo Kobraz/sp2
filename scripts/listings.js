16fcb93294ecbbf175b999c56efa1371d6574001
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-04 18:02:33 +0100

    upd dashboard.html and listings.js

--- scripts/listings.js
+++ scripts/listings.js
@@ -1,21 +1,56 @@
-fetch("https://api.noroff.dev/api/v1/auction/listings")
-  .then((response) => response.json())
-  .then((data) => {
-    const cardTemplate = document.querySelector(".card");
-    const tableBody = document.querySelector("#tableBody");
+function populateTable(data) {
+  const card = document.getElementById("card");
 
-    data.forEach((item) => {
-      const card = cardTemplate.cloneNode(true);
-      card.querySelector("#cardHeader").textContent = item.title;
-      card.querySelector("#cardDescription").textContent = item.description;
-      card.querySelector("#cardMedia").textContent = item.media;
-      card.querySelector("#cardTags").textContent = item.tags;
-      card.querySelector("#cardCreated").textContent = item.created;
-      card.querySelector("#cardUpdated").textContent = item.updated;
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card";
 
-      tableBody.appendChild(card);
-    });
-  })
-  .catch((error) => {
-    console.error("Error:", error);
+    const cardHeader = document.createElement("h2");
+    cardHeader.id = "cardHeader";
+    cardHeader.textContent = item.header;
+    card.appendChild(cardHeader);
+
+    const cardDescription = document.createElement("p");
+    cardDescription.id = "cardDescription";
+    cardDescription.textContent = item.description;
+    card.appendChild(cardDescription);
+
+    const cardMedia = document.createElement("p");
+    cardMedia.id = "cardMedia";
+    cardMedia.textContent = item.media;
+    card.appendChild(cardMedia);
+
+    const cardTags = document.createElement("p");
+    cardTags.id = "cardTags";
+    cardTags.textContent = item.tags;
+    card.appendChild(cardTags);
+
+    const cardCreated = document.createElement("p");
+    cardCreated.id = "cardCreated";
+    cardCreated.textContent = item.created;
+    card.appendChild(cardCreated);
+
+    const cardUpdated = document.createElement("p");
+    cardUpdated.id = "cardUpdated";
+    cardUpdated.textContent = item.updated;
+    card.appendChild(cardUpdated);
+
+    card.appendChild(card);
   });
+}
+
+// Function to fetch data from API and populate table
+function fetchAndPopulateTable() {
+  fetch("https://api.noroff.dev/api/v1/auction/listings")
+    .then((data) => {
+      return data.json();
+    })
+    .then((objectData) => {
+      populateTable(objectData);
+    });
+}
+
+// Run fetchAndPopulateTable function when the DOM has been loaded
+document.addEventListener("DOMContentLoaded", function () {
+  fetchAndPopulateTable();
+});
