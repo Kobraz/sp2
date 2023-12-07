4f093ee1c69a6fc10c5cff0c659ef002a5366f98
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-07 22:35:58 +0100

    upd

--- scripts/uploadAvatar.js
+++ scripts/uploadAvatar.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/* document.addEventListener("DOMContentLoaded", function () {
   const profileIdFromLocalStorage = localStorage.getItem("name");
   const profileId = profileIdFromLocalStorage.slice(1, -1);
   const accessToken = localStorage.getItem("accessToken");
@@ -36,7 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const result = await response.json();
       console.log("Avatar updated successfully:", result);
-    } catch (error) {
+    } 
+    catch (error) {
       throw new Error(`Error updating avatar: ${error.message}`);
     }
   }
@@ -46,4 +47,37 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 console.log("ProfileIDFromLocalStorage: " + profileIdFromLocalStorage);
-console.log("ProfileID (sliced): " + profileId);
+console.log("ProfileID (sliced): " + profileId); */
+
+/*-----------------------------------------------------------*/
+
+const apiURL = "https://api.noroff.dev/api/v1/auction/profiles/";
+const profileId = localStorage.getItem("name");
+
+function handleUnauthorized(response) {
+  if (response.status === 401) {
+    alert("You are not authorized to perform this action. Please log in and try again.");
+    window.location.href = "/login";
+  }
+}
+
+async function updateAvatar(data) {
+  const response = await fetch(apiURL + `${profileId}/media`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("accessToken"),
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    console.log("Avatar updated successfully:", result);
+  } else {
+    handleUnauthorized(response);
+    console.error(`Error updating avatar: ${response.statusText}`);
+    console.error(`Response: ${response}`);
+    return false;
+  }
+}
