b328a80828272f33a27f0a0d324a2deeb55f36d4
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-07 23:29:45 +0100

    upd

--- scripts/uploadAvatar.js
+++ scripts/uploadAvatar.js
@@ -5,8 +5,8 @@ console.log("ProfileIDFromLocalStorage: " + profileIdFromLocalStorage);
 console.log("ProfileID (sliced): " + profileId);
 
 document.addEventListener("DOMContentLoaded", function () {
-  /* const accessToken = localStorage.getItem("accessToken");*/
-  /* console.log("Token: " + accessToken); */
+  const accessToken = localStorage.getItem("accessToken");
+  console.log("Token: " + accessToken);
 });
 
 document.getElementById("form").addEventListener("submit", async function (event) {
