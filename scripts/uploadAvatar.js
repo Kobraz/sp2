6f7ac79ef95c6a5716164476ac4d9762c8a7f35d
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-07 16:20:20 +0100

    cp and upd uploadAvatar.js added avatar2.png

--- scripts/uploadAvatar.js
+++ scripts/uploadAvatar.js
@@ -1,20 +1,41 @@
+// Function to handle form submission
+document.getElementById("form").addEventListener("submit", async function (event) {
+  event.preventDefault(); // Prevent default form submission
+
+  // Get the value from the imgURL input field
+  const newAvatarUrl = document.getElementById("imgURL").value;
+
+  try {
+    // Call the updateAvatar function with the obtained URL
+    await updateAvatar(profileId, newAvatarUrl);
+  } catch (error) {
+    console.error("Error updating avatar:", error);
+    // Handle errors here
+  }
+});
+
 async function updateAvatar(profileId, newAvatarUrl) {
   const url = `https://api.noroff.dev/api/v1/auction/profiles/${profileId}`;
 
-  const response = await fetch(url, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ avatar: newAvatarUrl }),
-  });
+  try {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ avatar: newAvatarUrl }),
+    });
 
-  if (!response.ok) {
-    throw new Error(`HTTP error: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log("Avatar updated successfully:", result);
+  } catch (error) {
+    throw new Error(`Error updating avatar: ${error.message}`);
   }
-
-  const result = await response.json();
-  console.log("Avatar updated successfully:", result);
 }
 
-updateAvatar(profileId, newAvatarUrl);
+// Replace profileId with the actual profile ID
+const profileId = "your_profile_id_here";
