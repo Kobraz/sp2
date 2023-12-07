c4c1674e945f9cb399750906bee703fa9462ef24
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-07 16:41:24 +0100

    upd uploadAvatar.js

--- scripts/uploadAvatar.js
+++ scripts/uploadAvatar.js
@@ -1,42 +1,44 @@
-// Function to handle form submission
-document.getElementById("form").addEventListener("submit", async function (event) {
-  event.preventDefault(); // Prevent default form submission
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to handle form submission
+  document.getElementById("form").addEventListener("submit", async function (event) {
+    event.preventDefault(); // Prevent default form submission
 
-  // Get the value from the imgURL input field
-  const newAvatarUrl = document.getElementById("imgURL").value;
+    // Get the value from the imgURL input field
+    const newAvatarUrl = document.getElementById("imgURL").value;
 
-  try {
-    // Call the updateAvatar function with the obtained URL
-    await updateAvatar(profileId, newAvatarUrl);
-  } catch (error) {
-    console.error("Error updating avatar:", error);
-    // Handle errors here
-  }
-});
-
-async function updateAvatar(profileId, newAvatarUrl) {
-  const url = `https://api.noroff.dev/api/v1/auction/profiles/${profileId}`;
-
-  try {
-    const response = await fetch(url, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ avatar: newAvatarUrl }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
+    try {
+      // Call the updateAvatar function with the obtained URL
+      await updateAvatar(profileId, newAvatarUrl);
+    } catch (error) {
+      console.error("Error updating avatar:", error);
+      // Handle errors here
     }
+  });
 
-    const result = await response.json();
-    console.log("Avatar updated successfully:", result);
-  } catch (error) {
-    throw new Error(`Error updating avatar: ${error.message}`);
+  async function updateAvatar(profileId, newAvatarUrl) {
+    const url = `https://api.noroff.dev/api/v1/auction/profiles/${profileId}`;
+
+    try {
+      const response = await fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ avatar: newAvatarUrl }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log("Avatar updated successfully:", result);
+    } catch (error) {
+      throw new Error(`Error updating avatar: ${error.message}`);
+    }
   }
-}
 
-// Replace profileId with the actual profile ID
-const profileIdFromLocalStorage = localStorage.getItem("name");
-const profileId = profileIdFromLocalStorage.slice(1, -1);
+  // Replace profileId with the actual profile ID
+  const profileIdFromLocalStorage = localStorage.getItem("name");
+  const profileId = profileIdFromLocalStorage.slice(1, -1);
+});
