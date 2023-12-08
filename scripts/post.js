3411e5954f672a3b54e85d02b895ca5d1699c5bb
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-08 18:34:39 +0100

    upd post.html and created post.js

---
+++ scripts/post.js
@@ -0,0 +1,44 @@
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get form data
+  const formData = new FormData(document.getElementById("form"));
+
+  console.log("formData: " + formData);
+
+  // Convert FormData to JSON
+  const jsonFormData = {};
+  formData.forEach((value, key) => {
+    if (jsonFormData[key]) {
+      if (!Array.isArray(jsonFormData[key])) {
+        jsonFormData[key] = [jsonFormData[key]];
+      }
+      jsonFormData[key].push(value);
+    } else {
+      jsonFormData[key] = value;
+    }
+  });
+
+  // Make a POST request to the API
+  fetch("https://api.noroff.dev/api/v1/auction/listings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonFormData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Form data successfully sent:", data);
+      // Do something with the response data if needed
+    })
+    .catch((error) => {
+      console.error("There was a problem sending the form data:", error);
+      // Handle error
+    });
+});
