c54b9a18f05485b56ee12444f9395042a3621804
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-09 10:56:16 +0100

    upd

--- scripts/post.js
+++ scripts/post.js
@@ -9,14 +9,22 @@ document.getElementById("form").addEventListener("submit", function (event) {
   const postTitle = document.getElementById("postTitle").value;
   const postDescription = document.getElementById("postDescription").value;
   const postTags = document.getElementById("postTags").value;
-  const postImage = document.getElementById("postImage").value;
   const postDeadline = document.getElementById("postDeadline").value;
 
   console.log("Post Title: " + postTitle);
   console.log("Post Description: " + postDescription);
   console.log("Post Tags: " + postTags);
-  console.log("Post Image: " + postImage);
   console.log("Post Deadline: " + postDeadline);
+
+  document.getElementById("addImageButton").addEventListener("click", function () {
+    // Get all input elements with class 'postImage'
+    const inputs = document.querySelectorAll(".postImage");
+
+    // Log the values of all input elements
+    inputs.forEach(function (input) {
+      console.log(input.value);
+    });
+  });
 
   // Convert FormData to JSON
   const jsonFormData = {};
