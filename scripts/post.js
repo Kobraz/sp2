af524f90627d02f5c5124da3be3ec5c9cfce0580
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-09 11:10:41 +0100

    upd

--- scripts/post.js
+++ scripts/post.js
@@ -5,42 +5,40 @@ document.getElementById("form").addEventListener("submit", function (event) {
   const formData = new FormData(document.getElementById("form").value);
 
   console.log("formData: " + formData);
-
-  });
-
-  // Convert FormData to JSON
-  const jsonFormData = {};
-  formData.forEach((value, key) => {
-    if (jsonFormData[key]) {
-      if (!Array.isArray(jsonFormData[key])) {
-        jsonFormData[key] = [jsonFormData[key]];
-      }
-      jsonFormData[key].push(value);
-    } else {
-      jsonFormData[key] = value;
-    }
-  });
-
-  // Make a POST request to the API
-  fetch("https://api.noroff.dev/api/v1/auction/listings", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(jsonFormData),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Form data successfully sent:", data);
-      // Do something with the response data if needed
-    })
-    .catch((error) => {
-      console.error("There was a problem sending the form data:", error);
-      // Handle error
-    });
 });
+
+// Convert FormData to JSON
+const jsonFormData = {};
+formData.forEach((value, key) => {
+  if (jsonFormData[key]) {
+    if (!Array.isArray(jsonFormData[key])) {
+      jsonFormData[key] = [jsonFormData[key]];
+    }
+    jsonFormData[key].push(value);
+  } else {
+    jsonFormData[key] = value;
+  }
+});
+
+// Make a POST request to the API
+fetch("https://api.noroff.dev/api/v1/auction/listings", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(jsonFormData),
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Form data successfully sent:", data);
+    // Do something with the response data if needed
+  })
+  .catch((error) => {
+    console.error("There was a problem sending the form data:", error);
+    // Handle error
+  });
