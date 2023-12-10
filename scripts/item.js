24862639eabc9b434fb8971bf13561945458fbd5
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-10 14:17:26 +0100

    upd

---
+++ scripts/item.js
@@ -0,0 +1,35 @@
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function fetchAndDisplayItem() {
+  const itemId = getParameterByName("id");
+
+  fetch(`https://api.noroff.dev/api/v1/auction/listings/${itemId}`)
+    .then((data) => {
+      return data.json();
+    })
+    .then((objectData) => {
+      displayItem(objectData);
+    });
+}
+
+function displayItem(item) {
+  const itemContent = document.getElementById("itemContent");
+
+  // ... code to display the item details ...
+
+  itemContent.innerHTML = `
+       <h1>${item.title}</h1>
+       <p>${item.description}</p>
+       <img src="${item.image}" alt="${item.title}">
+       <p>Price: ${item.price} kr</p>
+    `;
+}
+
+fetchAndDisplayItem();
