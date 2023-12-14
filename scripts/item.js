422553e787fd84d6042dd6310a9f6e85fa38e674
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-14 13:23:58 +0100

    upd

--- scripts/item.js
+++ scripts/item.js
@@ -41,7 +41,7 @@ function displayItem(item) {
          <p>Updated: ${item.updated}</p>
          <p>Ends at: ${item.endsAt}</p>
          <p>Bids: ${item._count}</p>
-         <input type="number" id="bid" name="bid" min="0" max="1000"><button class="placeBid">Place bid</button>
+         <input type="number" id="bid" class="bid" name="bid" min="0" max="1000"><button class="placeBid">Place bid</button>
       `;
 }
 
