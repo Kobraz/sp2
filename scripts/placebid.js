80c70504086aadc0dec992a81f4cb12fe5e81ac3
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-14 17:59:52 +0100

    upd

--- scripts/placebid.js
+++ scripts/placebid.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const placeBidButton = document.querySelector(".placeBid");
   const bidInput = document.querySelector("#bid"); // Updated query selector for the bid input
 
+  const queryString = window.location.search;
+  const queryStringSliced = queryString.slice(4);
+  console.log("Query String:", queryString);
+  console.log("Query String Sliced:", queryStringSliced);
+
   placeBidButton.addEventListener("click", () => {
-    const itemId = item.id; // Assuming item is declared and holds the ID of the auction item
+    const itemId = queryStringSliced; // Assuming item is declared and holds the ID of the auction item
     const bidValue = parseFloat(bidInput.value);
 
     if (bidValue > 0) {
