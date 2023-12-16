1dd4a7779bbf6cbeb28eddb98e24bbaa314322fe
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-12-16 16:40:44 +0100

    upd

--- scripts/item.js
+++ scripts/item.js
@@ -1,4 +1,4 @@
-import { addBidEventListener } from "./placebid.mjs";
+import { addBidEventListener } from "../scripts/placeBid.mjs";
 
 function getParameterByName(name, url = window.location.href) {
   name = name.replace(/[\[\]]/g, "\\$&");
